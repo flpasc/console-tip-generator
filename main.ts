daba12bbe60440595e346fcf4b8e9bce087c3886
Daniel <daniel.seebacher17@gmail.com>
2025-07-21 15:20:26 +0200

    Initialized tipCalculator class and methods

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+class TipCalculator {
+  private checkAmount: number;
+  private tipPercentage: number;
+  private isDivided: boolean;
+  private numPeople: number;
+
+  constructor(
+    checkAmount: number,
+    tipPercentage: number,
+    isDivided: boolean,
+    numPeople: number
+  ) {
+    this.checkAmount = checkAmount;
+    this.tipPercentage = tipPercentage;
+    this.isDivided = isDivided;
+    this.numPeople = numPeople;
+  }
+
+  private calculateTip(checkAmount: number, tipPercentage: number): number {
+    return checkAmount * (tipPercentage / 100);
+  }
+
+  calculateTotal(tip: number, checkAmount: number): number {
+    return tip + checkAmount;
+  }
+
+  calculateAmountPerPerson(totalAmount: number, numPeople: number): number {
+    return parseFloat((totalAmount / numPeople).toFixed(2));
+  }
+}
+
 export function main() {
   const output: string = `
     --- Tip Calculation Summary ---
