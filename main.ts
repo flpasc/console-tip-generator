3074938947788174fb390cff300c97372fcaa877
Daniel <daniel.seebacher17@gmail.com>
2025-07-21 14:57:36 +0200

    Initialized project setup

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-export function main() {}
+export function main() {
+  const output: string = `
+    --- Tip Calculation Summary ---
+    Check Amount: $50.00
+    Tip Percentage: 15%
+    Tip Amount: $7.50
+    Total Bill: $57.50
+    Divide among people: yes
+    Split between how many people: 2
+    Each person pays: $28.75
+    -----------------------------`;
+  return output;
+}
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,7 @@
-test('Expected Final putput', () => {
-	expect(main()).toBe(`
+import { main } from "../main";
+
+test("Expected Final putput", () => {
+  expect(main()).toBe(`
     --- Tip Calculation Summary ---
     Check Amount: $50.00
     Tip Percentage: 15%
@@ -8,5 +10,5 @@ test('Expected Final putput', () => {
     Divide among people: yes
     Split between how many people: 2
     Each person pays: $28.75
-    -----------------------------`)
-})
+    -----------------------------`);
+});
