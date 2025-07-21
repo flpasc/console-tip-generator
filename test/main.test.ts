87f473258f97088b2622dfe4e56fec4eafed1b5d
Florian <florianpaulschoene@gmail.com>
2025-07-21 14:27:17 +0200

    init

---
+++ test/main.test.ts
@@ -0,0 +1,12 @@
+test('Expected Final putput', () => {
+	expect(main()).toBe(`
+    --- Tip Calculation Summary ---
+    Check Amount: $50.00
+    Tip Percentage: 15%
+    Tip Amount: $7.50
+    Total Bill: $57.50
+    Divide among people: yes
+    Split between how many people: 2
+    Each person pays: $28.75
+    -----------------------------`)
+})
